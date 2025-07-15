4fa664c455f7c5d485218d0eb3c12bbf1d0874fc
ciel <enberkaych@gmail.com>
2025-07-15 17:17:16 +0700

    ได้แก้ไขเส้นทาง import ของ UploadFile ใน Register.jsx

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -6,7 +6,7 @@ import useAuthStore from "../../store/auth-store";
 import { useNavigate } from "react-router-dom"
 import { listCustomerType } from "../../api/customerType"
 const API_URL = import.meta.env.VITE_API_URL
-import UploadFile from "../shared/UploadFile"
+import UploadFile from "../../components/shared/UploadFile"
 import image from "../../assets/Images/test3.png"
 
 const Register = () => {
