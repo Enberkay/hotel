95c18b95a6f5827148855992e7b3d44796b567fd
ciel <enberkaych@gmail.com>
2025-07-20 21:32:58 +0700

    feat: update frontend to display RoomType name based on selected language

--- server/controllers/roomType.js
+++ server/controllers/roomType.js
@@ -2,15 +2,15 @@ const prisma = require("../config/prisma")
 
 exports.create = async (req, res) => {
     try {
-
-        const { roomTypeName, price } = req.body
+        const { roomTypeName, price, name_en, name_th } = req.body;
         const roomType = await prisma.roomType.create({
             data: {
-                roomTypeName: roomTypeName,
+                roomTypeName: roomTypeName || name_th || name_en,
+                name_en: name_en || roomTypeName,
+                name_th: name_th || roomTypeName,
                 price: Number(price)
             }
         })
-
         res.json(roomType)
     } catch (err) {
         console.log(err)
@@ -44,11 +44,11 @@ exports.read = async (req, res) => {
 
 exports.update = async (req, res) => {
     try {
-        const { roomTypeName, price } = req.body
+        const { roomTypeName, price, name_en, name_th } = req.body;
         const roomTypeId = Number(req.params.id)
 
-        if (!roomTypeName || !price) {
-            return res.status(400).json({ message: "ไม่มีข้อมูล" })
+        if ((!roomTypeName && !name_en && !name_th) || !price) {
+            return res.status(400).json({ message: "ไม่มีข้อมูลที่ต้องการ" })
         }
 
         const roomTypeUpdated = await prisma.roomType.update({
@@ -56,7 +56,9 @@ exports.update = async (req, res) => {
                 roomTypeId: roomTypeId
             },
             data: {
-                roomTypeName: roomTypeName,
+                roomTypeName: roomTypeName || name_th || name_en,
+                name_en: name_en || roomTypeName,
+                name_th: name_th || roomTypeName,
                 price: price
             }
         })
@@ -71,7 +73,6 @@ exports.update = async (req, res) => {
 
 exports.remove = async (req, res) => {
     try {
-
         const { id } = req.params
         const roomType = await prisma.roomType.delete({
             where: {
